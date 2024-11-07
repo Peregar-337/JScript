6c40f7d084742f8648bf26ca3f7ffb3a66cd85db
Peregar-337 <Kirya.seryogin79@mail.ru>
2024-11-08 00:21:56 +1000

    Add files via upload

---
+++ 66/script.js
@@ -0,0 +1,37 @@
+gsap.to('#beach',{
+    scrollTrigger : {
+        scrub: 1
+    },
+    scale: 1.5
+})
+
+gsap.to('#cloud', {
+    scrollTrigger : {
+        scrub: 1.4
+    },
+    y: -500
+})
+
+gsap.to('#palma_right', {
+    scrollTrigger : {
+        scrub: 1
+    },
+    x: -500
+})
+
+
+gsap.to('#palma__left', {
+    scrollTrigger : {
+        scrub: 1
+    },
+    x: 500
+})
+
+gsap.to('#text', {
+    scrollTrigger : {
+        scrub: 1.5
+    },
+    y: 300
+})
+
+
